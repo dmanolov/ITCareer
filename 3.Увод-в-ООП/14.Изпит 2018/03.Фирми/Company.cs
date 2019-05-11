ba18c471391666fb142cb936ccc1860173227494
dmanolov <1045522+dmanolov@users.noreply.github.com>
2019-05-11 12:45:04 +0300

    задачите от 11.05.2019

---
+++ 3.Увод-в-ООП/14.Изпит 2018/03.Фирми/Company.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Фирми
+{
+    class Company
+    {
+        private string name;
+        private List<Employee> employees;
+        private string city;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (value.Length > 2 )
+                    name = value;
+                else throw new System.ArgumentException("Invalid company name");
+            }
+        }
+        public string City
+        {
+            get => city;
+            set
+            {
+                if (value.Length > 4 && char.IsUpper(value[0])) 
+                    city = value;
+                else throw new System.ArgumentException("Invalid city");
+            }
+
+        }
+
+        public Company(string name, string city)
+        {
+            this.Name = name;
+            this.employees = new List<Employee>();
+            this.City = city;
+        }
+
+        public override string ToString()
+        {
+            string result = $"Company {name} from {city} has the following employees:" + Environment.NewLine;
+            foreach (var e in employees)
+            {
+                result = result + "--->" + e.ToString() + Environment.NewLine;
+            }
+
+            return result;
+        }
+
+        public void HireEmployee(Employee emp)
+        {
+            employees.Add(emp);
+        }
+
+        public void FireEmployee(string employeeId)
+        {
+            foreach (var e in employees)
+            {
+                if(e.Id == employeeId)
+                {
+                    employees.Remove(e);
+                    break;
+                }
+
+            }
+        }
+
+        public void IncreaseSalaries(double value)
+        {
+            foreach (var e in employees)
+            {
+                e.Salary += value;
+            }
+        }
+
+        public void DecreaseSalaries(double value)
+        {
+            foreach (var e in employees)
+            {
+                if(e.Salary >= value)
+                    e.Salary -= value;
+            }
+        }
+
+        public Employee GetMostHighPaidEmployee()
+        {
+            double maxSalary = 0;
+            Employee mostPaidEmployee = null;
+            foreach (var e in employees)
+            {
+                if (e.Salary > maxSalary)
+                {
+                    maxSalary = e.Salary;
+                    mostPaidEmployee = e;
+                }
+            }
+            return mostPaidEmployee;
+        }
+
+        public List<Employee> GetTopThreeMostHighPaidEmployees()
+        {
+            return employees.OrderByDescending(e => e.Salary).Take(3).ToList();
+        }
+
+        public bool CheckEmployeeIsPresent(string employeeId)
+        {
+            bool found = false;
+            foreach (var e in employees)
+            {
+                if (e.Id == employeeId)
+                {
+                    found = true;
+                    break;
+                }
+            }
+            return found;
+        }
+
+        public double GetAverageEmployeeSalary()
+        {
+            return employees.Average(e => e.Salary);
+        }
+
+    }
+}
---
+++ 3.Увод-в-ООП/14.Изпит 2018/03.Фирми/Employee.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Фирми
+{
+    class Employee
+    {
+        private string id;
+        private string firstName;
+        private string lastName;
+        private int age;
+        private double salary;
+
+        public string Id
+        {
+            get => id;
+        }
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                if (value.Length > 2 && value.Length < 8)
+                    firstName = value;
+                else throw new System.ArgumentException("Invalid employee name");
+            }
+        }
+        public string LastName
+        {
+            get => lastName;
+        }
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (value > 0)
+                    age = value;
+                else throw new System.ArgumentException("Invalid employee age");
+            }
+        }
+        public double Salary
+        {
+            get => salary;
+            set => salary = value;
+        }
+
+        public Employee(string id, string firstName, string lastName, int age)
+            : this(id, firstName, lastName, age, 500.0)
+        {
+        }
+
+        public Employee(string id, string firstName, string lastName, int age, double salary)
+            
+        {
+            this.id = id;
+            this.FirstName = firstName;
+            this.lastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"Employee: {firstName} {lastName} with id: {id} and salary: {salary:F2}";
+        }
+
+
+    }
+}
