79bd342a0b9f805104463de72a3e89acedf76738
dmanolov <1045522+dmanolov@users.noreply.github.com>
2019-06-23 13:25:53 +0300

    задачите от 23.06.2019

---
+++ 4.Увод-в-Алгоритмите/05.1.Алгоритми-за-търсене/Problem 1. Линейно търсене/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1.Линейно_търсене
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int key = int.Parse(Console.ReadLine());
+            // извикваме метода за линейно търсене
+            Console.WriteLine(LinearSearch(arr, key));
+            // същото, написано директно в кода
+            int keyIndex = -1; // отначало решаваме, че го няма
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == key)
+                {
+                    keyIndex = i;   // запомняме на коя позиция е
+                    break;          // няма смисъл да търсим нататък
+                }
+            }
+            Console.WriteLine(keyIndex);
+        }
+
+        static int LinearSearch(int[] arr, int key)
+        {
+            // обхождаме масива
+            for (int i = 0; i < arr.Length; i++)
+            {
+                // ако намериме числото, връщаме индекса
+                if (arr[i] == key)
+                {
+                    return i;
+                }
+            }
+            // като завърши цикъла, връщаме, че го няма
+            return -1;
+        }
+    }
+}
---
+++ 4.Увод-в-Алгоритмите/05.1.Алгоритми-за-търсене/Problem 2. Двоично търсене/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Двоично_търсене
+{
+    class Program
+    {
+        static int keyNotFound = -1;
+
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int key = int.Parse(Console.ReadLine());
+            // извикваме методите за двоично търсене
+            Console.WriteLine(BinarySearchI(arr, key, 0, arr.Length - 1));
+            Console.WriteLine(BinarySearchR(arr, key, 0, arr.Length - 1));
+        }
+
+        // двоично търсене - вариант с итерации
+        static int BinarySearchI(int[] arr, int key, int start, int end)
+        {
+            while (end >= start)
+            {
+                int mid = (start + end) / 2;
+                if (arr[mid] < key)
+                    start = mid + 1;
+                else if (arr[mid] > key)
+                    end = mid - 1;
+                else
+                    return mid;
+            }
+            return keyNotFound;
+        }
+
+        // двоично търсене - рекурсивен вариант
+        static int BinarySearchR(int[] arr, int key, int start, int end)
+        {
+            if (end < start)
+                return keyNotFound;
+            else
+            {
+                int mid = (start + end) / 2;
+                if (arr[mid] > key)
+                    return BinarySearchR(arr, key, start, mid - 1);
+                else if (arr[mid] < key)
+                    return BinarySearchR(arr, key, mid + 1, end);
+                else
+                    return mid;
+            }
+        }
+
+    }
+}
