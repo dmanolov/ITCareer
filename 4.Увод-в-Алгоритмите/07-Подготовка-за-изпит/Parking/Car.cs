8e384d52206983a9a9f17e9bc7c337fc046232ad
dmanolov <1045522+dmanolov@users.noreply.github.com>
2019-07-07 14:06:15 +0300

    задачите от 07.07.2019

---
+++ 4.Увод-в-Алгоритмите/07-Подготовка-за-изпит/Parking/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking
+{
+    public class Car
+    {
+        private string carNumber;
+        private Car next;
+
+        public Car(string carNumber)
+        {
+            this.carNumber = carNumber;
+            this.next = null;
+        }
+
+        public string CarNumber
+        {
+            get => carNumber;
+            set => carNumber = value;
+        }
+
+        public Car Next
+        {
+            get => next;
+            set => next = value;
+        }
+
+        public override string ToString()
+        {
+            return $"Car: {carNumber}";
+        }
+    }
+}
---
+++ 4.Увод-в-Алгоритмите/07-Подготовка-за-изпит/Parking/Parking.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking
+{
+    public class Parking
+    {
+        private Car head;
+        private Car tail;
+        private int count;
+
+        public int Count
+        {
+            get => count;
+            private set => count = value;
+        }
+
+        public Parking()
+        {
+            head = tail = null;
+            count = 0;
+        }
+
+        public void AddCar(string carNumber)
+        {
+            Car newCar = new Car(carNumber);
+            if (Count == 0)
+            {
+                head = tail = newCar;
+            }
+            else
+            {
+                tail.Next = newCar;
+                tail = newCar;
+            }
+            count++;
+        }
+
+        public void AddFancyCar(string carNumber)
+        {
+            Car newCar = new Car(carNumber);
+            if (Count == 0)
+            {
+                head = tail = newCar;
+            }
+            else
+            {
+                newCar.Next = head;
+                head = newCar;
+            }
+            count++;
+        }
+
+        public Car CheckCarIsPresent(string carNumber)
+        {
+            Car current = head;
+            while (current != null)
+            {
+                if (carNumber == current.CarNumber)
+                {
+                    return current;
+                }
+                current = current.Next;
+            }
+            return null;
+        }
+
+        public bool ReleaseCar(string carNumber)
+        {
+            if (Count == 0)
+                return false;
+            // ако махаме първата кола
+            if (head.CarNumber == carNumber)
+            {
+                Car removed = head;
+                head = head.Next;
+                removed.Next = null;
+                count--;
+                return true;
+            }
+            else
+            {
+                // намираме предишната на търсената
+                Car current = head;
+                while (current.Next != null && current.Next.CarNumber != carNumber)
+                    current = current.Next;
+                // ако сме стигнали до края, значи няма такава
+                if (current.Next == null)
+                    return false;
+                // или следващата е тази, която ще премахваме
+                Car removed = current.Next;
+                // пропускаме премахваната и сочим към следващата след нея
+                current.Next = removed.Next;
+                // ако е бил последна, новата последна е и опашка
+                if (removed.Next == null)
+                    tail = current;
+                removed.Next = null;
+                count--;
+                return true;
+            }
+        }
+
+        public bool ReleaseCar(int index)
+        {
+            // проверяваме всички проблемни случаи
+            if (index < 0 || index >= Count)
+                return false;
+            if (index == 0)
+            {
+                Car removed = head;
+                head = head.Next;
+                removed.Next = null;
+                count--;
+                return true;
+            }
+            else
+            {
+                // намираме предишната на премахваната
+                Car current = head;
+                for (int i = 0; i < (index - 1); i++)
+                    current = current.Next;
+                Car removed = current.Next;
+                // пропускаме премахваната и сочим към следващата след нея
+                current.Next = removed.Next;
+                // ако е бил последна, новата последна е и опашка
+                if (removed.Next == null)
+                    tail = current;
+                removed.Next = null;
+                count--;
+                return true;
+            }
+        }
+
+        public StringBuilder ParkingInformation()
+        {
+            StringBuilder builder = new StringBuilder();
+            if (Count == 0)
+            {
+                builder.AppendLine("< Parking is empty!>");
+            }
+            else
+            {
+                Car current = head;
+                for (int i = 0; i < Count; i++)
+                {
+                    builder.AppendLine(current.ToString());
+                    current = current.Next;
+                }
+            }
+            return builder;
+        }
+
+    }
+}
